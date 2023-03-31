f29ff8551658b0c80ba50809edcee6aa2786c153
Roberto Sartori <roberto.sartori.android@gmail.com>
2023-03-31 22:30:28 +0200

    msm8998-common: move from the vendor folder the netmgr_config file
    We need to modify this for 4.14, so add (as a copy) from the
    vendor/oneplus repo.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- common.mk
+++ common.mk
@@ -460,3 +460,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# RmNet xml config file
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/data/netmgr_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/data/netmgr_config.xml
