TheRomFactory/android_device_oneplus_msm8998-common

msm8998-common: use hotword enrollment on gapps builds

common.mk
@@ -25,6 +25,11 @@
 $(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 
+ifeq ($(WITH_GAPPS), true)
+    # Get non-open-source specific aspects for Ok Google
+    $(call inherit-product, vendor/oneplus/google/google.mk)
+endif
+
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_COMPRESSED_APEX := false

nkeor/4.14-device_oneplus_msm8998-common

Signed-off-by: Namkhai B <me@forkbomb9.ch>

common.mk
@@ -6,6 +6,11 @@
 
 $(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
 
+ifeq ($(WITH_GMS), true)
+    # Get non-open-source specific aspects for Ok Google
+    $(call inherit-product, vendor/oneplus/google/google.mk)
+endif
+
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 PRODUCT_COMPRESSED_APEX := false